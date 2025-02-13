c3c0d8afa2dfe6641d9435d841627393ac56778d
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-02-13 14:47:45 -0800

    App functional

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+const orders = [];
+
+app.use(express.urelencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render('home');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
